9ead71e624bf94a21c30b530ab33495637f9ccc8
20ml <milliliter.avc@gmail.com>
2025-05-08 12:39:33 -0400

    Alter automation strategy to be better for startup uses

--- HideFromTaskbar.cs
+++ HideFromTaskbar.cs
@@ -6,13 +6,42 @@ using System.Runtime.InteropServices;
 using MelonLoader;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(DesktopMate_HideFromTaskbarMod.HideFromTaskbar), "DesktopMate-HideFromTaskbar", "1.0.0", "20ml", null)]
+[assembly: MelonInfo(typeof(DesktopMate_HideFromTaskbarMod.HideFromTaskbar), "DesktopMate-HideFromTaskbar", "1.0.1", "20ml", null)]
 [assembly: MelonGame("infiniteloop", "DesktopMate")]
 
 namespace DesktopMate_HideFromTaskbarMod
 {
     public class HideFromTaskbar : MelonMod
     {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+        
+        private bool awaitingFocus = false;
+        private bool wasFocused = false;
+        private bool hasHidden = false;
+        public override void OnUpdate()
+        {
+            if (hasHidden) { return; }
+
+            wasFocused = Input.GetMouseButtonDown(0);
+            if (wasFocused)
+            {
+                LoggerInstance.Msg("DesktopMate was focused.");
+            }
+            if (awaitingFocus && wasFocused) {
+                HideActiveWindowFromTaskbar();
+            }
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            awaitingFocus = true;
+            LoggerInstance.Msg("Waiting for window focus to hide DesktopMate from taskbar.");
+        }
+
+        // Code from https://discussions.unity.com/t/can-the-taskbar-icon-of-a-unity-game-be-hidden/790409/3
         [DllImport("user32.dll")]
         static extern IntPtr GetActiveWindow();
         [DllImport("User32.dll")]
@@ -23,29 +52,12 @@ namespace DesktopMate_HideFromTaskbarMod
         private const int GWL_EXSTYLE = -0x14;
         private const int WS_EX_TOOLWINDOW = 0x0080;
 
-        public override void OnInitializeMelon()
-        {
-            LoggerInstance.Msg("Initialized.");
-        }
-        
-        public override void OnUpdate()
-        {
-            if (Input.GetKeyDown(KeyCode.F8))
-            {
-                HideActiveWindowFromTaskbar();
-            }
-        }
-        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
-        {
-            HideActiveWindowFromTaskbar();
-        }
-
         public void HideActiveWindowFromTaskbar()
         {
-            // Code from https://discussions.unity.com/t/can-the-taskbar-icon-of-a-unity-game-be-hidden/790409/3
             LoggerInstance.Msg("Hiding DesktopMate from taskbar.");
             IntPtr pMainWindow = GetActiveWindow();
             SetWindowLong(pMainWindow, GWL_EXSTYLE, GetWindowLong(pMainWindow, GWL_EXSTYLE) | WS_EX_TOOLWINDOW);
+            hasHidden = true;
         }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This mod hides the primary DesktopMate window from the taskbar when it runs, to prevent taskbar clutter.
 
 ## Usage
-When DesktopMate's main scene is finished initializing, the mod will automatically attempt to hide the primary DesktopMate window from the taskbar.
 
-If this fails for some reason, "F8" can be pressed to retry.
+After launching DesktopMate, the mod will hide the primary DesktopMate window from the taskbar when it is first clicked.
+Setting window values will disrupt a drag action, so click first to hide the window, then drag the window.
